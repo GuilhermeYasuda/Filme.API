c3d1611e2ac965dbf10df0a785e40497994933e3
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-04-14 21:08:05 -0300

    feat: adicionar entidade base e filme

---
+++ Filme.API/Models/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Filme.API.Models
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; private init; } = Guid.NewGuid();
+        public DateTimeOffset DtCriacao { get; private set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset DtUltimaAlteracao { get; private set; } = DateTimeOffset.UtcNow;
+
+        public void AtualizarDataUltimaAlteracao()
+        {
+            DtUltimaAlteracao = DateTimeOffset.UtcNow;
+        }
+    }
+}
